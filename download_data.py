e5015bb6cc8154926a3bb341d83e68ffc1f79ef9
Joshua Omondi <joshuaomondi3334@gmail.com>
2025-05-18 16:53:31 +0300

    changes, done

--- download_data.py
+++ download_data.py
@@ -1,14 +1,20 @@
-import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
-os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
+
+# Debug print
+print("Loaded Username:", os.getenv("KAGGLE_USERNAME"))
+print("Loaded Key:", os.getenv("KAGGLE_KEY")[:4], "...")
+
+os.environ["KAGGLE_USERNAME"] = os.getenv("KAGGLE_USERNAME")
+os.environ["KAGGLE_KEY"] = os.getenv("KAGGLE_KEY")
 
 api = KaggleApi()
 api.authenticate()
 
 dataset = "vjchoudhary7/customer-segmentation-tutorial"
+print("Dataset URL: https://www.kaggle.com/datasets/vjchoudhary7/customer-segmentation-tutorial")
 api.dataset_download_files(dataset, path="data", unzip=True)
-print("Dataset downloaded and extracted.")
+print("✅ Dataset downloaded and extracted.")
